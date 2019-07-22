f0712eae70e860d881322ff8031889dba262a6e5
Yoshi Askharoun <jjask7@gmail.com>
2019-07-22 14:29:22 -0700

    Update MTAHelper.cs

--- MTATransit/MTATransit.Shared/API/MTA/MTAHelper.cs
+++ MTATransit/MTATransit.Shared/API/MTA/MTAHelper.cs
@@ -168,5 +168,15 @@ namespace MTATransit.Shared.API.MTA
                 return false;
             }
         }
+        
+        public static GetVIDFromBus() {
+            string url = "http://www.contoso.com:8080/letters/readme.html";
+
+          Regex r = new Regex(@"^(?<proto>\w+)://[^/]+?(?<port>:\d+)?/",
+                              RegexOptions.None, TimeSpan.FromMilliseconds(150));
+          Match m = r.Match(url);
+          if (m.Success)
+             Console.WriteLine(r.Match(url).Result("${proto}${port}")); 
+        }
     }
 }
