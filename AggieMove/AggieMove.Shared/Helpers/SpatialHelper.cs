f5c41014a8692282ebaaf1d06ae9da35a7d61e1d
Joshua Askharoun <jjask7@gmail.com>
2020-09-25 22:50:56 -0500

    Bind and show available routes

---
+++ AggieMove/AggieMove.Shared/Helpers/SpatialHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+
+namespace AggieMove.Helpers
+{
+    public static class SpatialHelper
+    {
+        public static double GetDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var R = 6371; // Radius of the earth in km
+            var dLat = ToRadians(lat2 - lat1);  // deg2rad below
+            var dLon = ToRadians(lon2 - lon1);
+            var a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+
+        public static double ToRadians(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+
+        public static bool IsWithinRadius(double lat, double lon, double x, double y, double radius)
+        {
+            return GetDistance(lat, lon, x, y) <= radius;
+        }
+
+        public static Geolocator Geolocator { get; internal set; }
+        private static Point CurrentLocationCache { get; set; }
+        private static DateTime? CurrentLocationLastUpdated { get; set; }
+        public static Point GetCachedLocation()
+        {
+            return CurrentLocationCache;
+        }
+        public async static System.Threading.Tasks.Task<Point> GetCurrentLocation(bool acceptCache = true)
+        {
+            if (CurrentLocationCache == null || !CurrentLocationLastUpdated.HasValue
+                || DateTime.Now.Subtract(CurrentLocationLastUpdated.Value).TotalMinutes > 5
+                || !acceptCache)
+            {
+                // Cache needs to be updated
+                var accessStatus = await Geolocator.RequestAccessAsync();
+                if (accessStatus == GeolocationAccessStatus.Allowed)
+                {
+                    double x, y;
+                    Geolocator = new Geolocator { DesiredAccuracyInMeters = 1 };
+                    Geoposition pos = await Geolocator.GetGeopositionAsync();
+                    y = pos.Coordinate.Point.Position.Latitude;
+                    x = pos.Coordinate.Point.Position.Longitude;
+                    CurrentLocationCache = new Point(x, y);
+                }
+            }
+
+            return CurrentLocationCache;
+        }
+    }
+}
--- AggieMove/AggieMove.Shared/Views/ExploreView.xaml.cs
+++ AggieMove/AggieMove.Shared/Views/ExploreView.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿using AggieMove.Helpers;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Collections.ObjectModel;
+using TamuBusFeed;
+using TamuBusFeed.Models;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +22,76 @@ namespace AggieMove.Views
 	/// </summary>
 	public sealed partial class ExploreView : Page
 	{
-		public ExploreView()
+        public ObservableCollection<Route> Routes = new ObservableCollection<Route>();
+
+        public ExploreView()
 		{
 			this.InitializeComponent();
 		}
-	}
+
+		private async void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+            Point currentLoc = await SpatialHelper.GetCurrentLocation();
+            LoadMap(currentLoc.Y, currentLoc.X);
+
+            SpatialHelper.Geolocator.PositionChanged += Geolocator_PositionChanged;
+
+            Routes.Clear();
+            foreach (Route r in await TamuBusFeedApi.GetRoutes())
+			{
+                Routes.Add(r);
+			}
+        }
+
+        private async void Geolocator_PositionChanged(Windows.Devices.Geolocation.Geolocator sender, Windows.Devices.Geolocation.PositionChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                MapGraphics.Graphics.Clear();
+
+                var stopPoint = CreateRouteStop(
+                    args.Position.Coordinate.Point.Position.Latitude,
+                    args.Position.Coordinate.Point.Position.Longitude,
+                    System.Drawing.Color.Red
+                );
+                MapGraphics.Graphics.Add(stopPoint);
+            });
+        }
+
+        public void LoadMap(double lat, double lon)
+        {
+            MainMapView.Map = new Map(
+                BasemapType.ImageryWithLabels,
+                lat,
+                lon,
+                12
+            );
+
+			// Now draw a point where the stop is
+			var stopPoint = CreateRouteStop(lat, lon, System.Drawing.Color.Red);
+			MapGraphics.Graphics.Add(stopPoint);
+
+			// Display all buildings
+			var buildingsAUri = new Uri("https://gis.tamu.edu/arcgis/rest/services/FCOR/TAMU_BaseMap/MapServer/2");
+			var buildingsALayer = new FeatureLayer(new ServiceFeatureTable(buildingsAUri));
+			MainMapView.Map.OperationalLayers.Add(buildingsALayer);
+			var buildingsBUri = new Uri("https://gis.tamu.edu/arcgis/rest/services/FCOR/TAMU_BaseMap/MapServer/3");
+			var buildingsBLayer = new FeatureLayer(new ServiceFeatureTable(buildingsBUri));
+			MainMapView.Map.OperationalLayers.Add(buildingsBLayer);
+		}
+
+        private Graphic CreateRouteStop(double lat, double lon, System.Drawing.Color fill)
+        {
+            // Now draw a point where the stop is
+            var mapPoint = new MapPoint(Convert.ToDouble(lon),
+                Convert.ToDouble(lat), SpatialReferences.Wgs84);
+            var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, fill, 20);
+            pointSymbol.Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.White, 5);
+            return new Graphic(mapPoint, pointSymbol);
+        }
+        private Graphic CreateInactiveRouteStop(double lat, double lon)
+        {
+            return CreateRouteStop(lat, lon, System.Drawing.Color.Black);
+        }
+    }
 }
